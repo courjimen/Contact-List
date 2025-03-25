fdb7e8798919d2974ecc2a83ba5399628eed4ff0
courjimen <coureyfrance@gmail.com>
2025-03-25 06:59:35 -0400

    added ;

--- server/db.sql
+++ server/db.sql
@@ -8,9 +8,9 @@ CREATE TABLE "contact list" (
     email VARCHAR(100),
     phone VARCHAR(25) UNIQUE NOT NULL,
     notes TEXT
-)
+);
 
 INSERT INTO "contact list" (first_name, last_name, email, phone, notes) VALUES 
 ('Mi', 'Madre 💕', 'momsemail@hotmail.com', '313-485-4495', 'Birthday: April 8'),
 ('Hub', 'Wub ♥️', 'hubby@gmail.com', '813-397-7791', 'Anniversary: May 7 ✨'),
-('Tre', NULL, 'trevonsofly@aol.com', '248-254-2020', 'Birthday: Marzo 27')
+('Tre', NULL, 'trevonsofly@aol.com', '248-254-2020', 'Birthday: Marzo 27');
