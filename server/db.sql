31fb127fde62a1df1cee754595eba954321225f1
courjimen <coureyfrance@gmail.com>
2025-03-25 18:30:01 -0400

    created favorites table

--- server/db.sql
+++ server/db.sql
@@ -19,3 +19,19 @@ INSERT INTO "contact list" (first_name, last_name, email, phone, notes) VALUES
 ('Mi', 'Madre 💕', 'momsemail@hotmail.com', '313-485-4495', 'Birthday: April 8'),
 ('Hub', 'Wub ♥️', 'hubby@gmail.com', '813-397-7791', 'Anniversary: May 7 ✨'),
 ('Tre', NULL, 'trevonsofly@aol.com', '248-254-2020', 'Birthday: Marzo 27');
+
+--Step 5 (Create Favorites Table)--
+CREATE TABLE favorites (
+    favorite_id SERIAL PRIMARY KEY,
+    favorite_name TEXT NOT NULL,
+    favorite_number VARCHAR(25) REFERENCES "contact list"(phone)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    favorite_notes TEXT
+);
+
+--Step 6 (Insert Data)--
+INSERT INTO favorites (favorite_name, favorite_number, favorite_notes) VALUES 
+('Mi Madre 💕', '313-485-4495', 'Birthday: April 8'),
+('Hub Wub ♥️', '813-397-7791', 'Anniversary: May 7 ✨');
+
